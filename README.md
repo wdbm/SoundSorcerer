e38d4ba86fed6434406a34fd1eccf06f089e0333
Will Breaden Madden <wbm@protonmail.ch>
2024-05-03 19:05:47 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SoundSorcerer
-continuously set the audio output device according to a prioritised list of audio output devices
+
+![](SoundSorcerer.png)
+
+The script `SoundSorcerer` continuously sets the audio output device according to a prioritised list of audio devices defined in the file `config.py`.
---
+++ config.py
@@ -0,0 +1,6 @@
+priority_list = [
+    'alsa_output.usb-Nreal_Nreal_Air_A00018_42_04-00.iec958-stereo',
+    'alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.iec958-stereo',
+    'alsa_output.usb-Anker_Anker_PowerConf_B600_202012-00.iec958-stereo',
+    'alsa_output.pci-0000_00_1f.3.analog-stereo'
+]
