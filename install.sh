6b36a64b1427a5bd09be37e2656a367c1bfb8ed1
Will Breaden Madden <wbm@protonmail.ch>
2026-04-06 16:23:52 -0300

    update

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt install python3-tk desktop-file-utils
+
+chmod +x ./SoundSorcerer_GUI
+mkdir -p ${HOME}/.local/bin
+cp ./SoundSorcerer_GUI ${HOME}/.local/bin/SoundSorcerer_GUI
+
+mkdir -p ${HOME}/.local/share/icons/hicolor/scalable/apps
+cp ./SoundSorcerer.svg ${HOME}/.local/share/icons/hicolor/scalable/apps/
+gtk-update-icon-cache "${HOME}/.local/share/icons/hicolor" >/dev/null 2>&1 || true
+
+mkdir -p ${HOME}/.local/share/applications
+cat > "${HOME}/.local/share/applications/SoundSorcerer_GUI.desktop" <<EOF
+[Desktop Entry]
+Type=Application
+Name=ASoundSorcerer GUI
+Comment=GUI to select sound output device
+Exec=${HOME}/.local/bin/SoundSorcerer_GUI
+TryExec=${HOME}/.local/bin/SoundSorcerer_GUI
+Icon=SoundSorcerer
+Terminal=false
+Categories=Graphics;Viewer;
+MimeType=image/png;image/jpeg;image/gif;
+StartupNotify=true
+EOF
+
+update-desktop-database "${HOME}/.local/share/applications"
